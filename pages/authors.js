0336cc766addace9eb99d8966c45f00c028c9808
Robert <robert.mabry86@gmail.com>
2023-04-19 21:23:14 -0500

    uncuaght save of authors page file

--- pages/authors.js
+++ pages/authors.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
 import { getAuthors } from '../api/authorData';
@@ -14,9 +14,9 @@ export default function Authors() {
     getAuthors(user.uid).then(setAuthors);
   };
 
-  useEffect(() => {
-    getAllAuthors();
-  }, []);
+  // useEffect(() => {
+  //   getAllAuthors();
+  // }, []);
   return (
     <div className="text-center my-4">
       <Link href="/author/new" passHref>
